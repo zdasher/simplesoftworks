2d77c5d2d0e7a95a18b47bc7a85b10e04fbab1ef
zdasher <zackdasher@gmail.com>
2014-07-15 16:37:58 -0500

    Main- simplesoftworks.com
    html/css/js

---
+++ myscript.js
@@ -0,0 +1,16 @@
+/*
+
+var color = "red";
+//using javascript to identify certain parts of html
+/*var myDiv = document.getElementById('mydiv');
+myDiv.style.background = "black";
+myDiv.style.color = "#ffffff";
+
+var myVar;
+
+console.log(typeof myVar);
+console.log(myVar === undefined);
+*/
+
+
+console.log("Hello, World!")
